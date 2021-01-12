3e004fca986ca6a89a4863b5a591adcb21ba40ec
monseeligio <71795808+monseeligio@users.noreply.github.com>
2021-01-12 17:42:32 -0600

    Create ExamenU5.java

---
+++ ExamenU5.java
@@ -0,0 +1,19 @@
+
+package examenu5;
+
+/**
+ *
+ * @author gama
+ */
+public class ExamenU5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LeerArchivo instancia =new LeerArchivo();
+        instancia.Ordenar();
+    }
+    
+}
